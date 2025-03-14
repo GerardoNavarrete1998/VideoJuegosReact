8e4b7cf53e1eacc4a858ad744e3dea47fb399e75
GerardoNavarrete1998 <gerardojarethnavarreten@gmail.com>
2025-03-14 00:18:05 -0300

    Filtros mas extensos

--- VideoJuegos/src/components/Filters.jsx
+++ VideoJuegos/src/components/Filters.jsx
@@ -20,6 +20,20 @@ const Filters = ({ onFilter }) => {
         <option value="adventure">Aventura</option>
         <option value="role-playing-games-rpg">RPG</option>
         <option value="strategy">Estrategia</option>
+        <option value="shooter">Shooter</option>
+        <option value="puzzle">Puzzle</option>
+        <option value="racing">Carreras</option>
+        <option value="sports">Deportes</option>
+        <option value="fighting">Peleas</option>
+        <option value="simulation">Simulación</option>
+        <option value="arcade">Arcade</option>
+        <option value="platformer">Plataformas</option>
+        <option value="card">Cartas</option>
+        <option value="family">Familiar</option>
+        <option value="massively-multiplayer">Multijugador Masivo</option>
+        <option value="board-games">Juegos de Mesa</option>
+        <option value="indie">Indie</option>
+
       </select>
 
       {/* Filtro por Plataformas (Usando IDs) */}
@@ -30,8 +44,16 @@ const Filters = ({ onFilter }) => {
         <option value="">Plataformas</option>
         <option value="4">PC</option>
         <option value="187,18,16,15,27,17,19">PlayStation</option>
-        <option value="3">Xbox</option>
-        <option value="7">Nintendo</option>
+        <option value="1,186,14">Xbox</option>
+        <option value="7,8,9,13,10,11,105,83,24,43,26,80,49">Nintendo</option>
+        <option value="171">Web</option>
+        <option value="5">macOS</option>
+        <option value="6">Linux</option>
+        <option value="34">Android</option>
+        <option value="21">iOS</option>
+        <option value="28,31,23,22,25,50,51,112,113,77,111">Atari</option>
+        <option value="166,167,107,119,106,117,74">SEGA</option>
+        <option value="111,77,12,79">Otros</option>
       </select>
 
       {/* Filtro por Tags */}
