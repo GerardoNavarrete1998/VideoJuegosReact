c88590a0b5339bc74742799d6a02b537c95877ba
GerardoNavarrete1998 <gerardojarethnavarreten@gmail.com>
2025-03-14 13:04:26 -0300

    Imagen de respaldo por falla de carga

--- VideoJuegos/src/components/GameCard.jsx
+++ VideoJuegos/src/components/GameCard.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 const GameCard = ({ game }) => {
   const navigate = useNavigate();
+  const fallbackImage = "/images/fallback.jpg";
 
   const handleClick = () => {
     navigate(`/game/${game.id}`);
@@ -9,7 +10,11 @@ const GameCard = ({ game }) => {
     //Juegos recibido por la api y mostrado en HOME con forma de cards
   return (
     <div className="game-card" onClick={handleClick}>
-      <img src={game.background_image} alt={game.name} />
+        <img
+        src={game.background_image || fallbackImage} // Usa la imagen del juego o la de respaldo
+        alt={game.name}
+        onError={(e) => (e.target.src = fallbackImage)} // Si la imagen no carga, usa la de respaldo
+      />
       <h2>{game.name}</h2>
       <p>Puntuación: {game.metacritic}</p>
     </div>
